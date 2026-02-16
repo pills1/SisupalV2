faad594c40eb0e0243753f56b6b34e2d61d936b9
Thanuji Dissanayake <thanujidissanayake2005@gmail.com>
2026-02-16 07:18:11 +0530

    Fix: Remove Web keys to fix Android startup freeze

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,7 @@ import 'providers/theme_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase for WEB with manual options
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyAHn3e3YIOH-TvQOL6HF-JTvwXXH97tBS4",
-        authDomain: "sisupal-782d3.firebaseapp.com",
-        projectId: "sisupal-782d3",
-        storageBucket: "sisupal-782d3.firebasestorage.app",
-        messagingSenderId: "213942877787",
-        appId: "1:213942877787:web:814aadc8f8fd5e523a497e"
-    ),
-  );
+  await Firebase.initializeApp();
 
   runApp(
     ChangeNotifierProvider(
